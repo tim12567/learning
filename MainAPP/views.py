5c08502b4f5cb141b8e97f5249c2bdf8f8cc3650
Timofey <hunsolo94@gmail.com>
2024-12-02 13:50:39 +0300

    Done part 1,2.

--- MainAPP/views.py
+++ MainAPP/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# def drive(request):
+#     text = '''<h1>"Изучаем django"</h1>
+# <strong>Автор</strong>: <i>Пармон Т.С.</i><br>
+# <i>Имя: <strong>Тимофей</strong></i><br>
+# <i>Отчество: <strong>Сергеевич</strong></i><br>
+# <i>Фамилия: <strong>Пармон</strong></i><br>
+# <i>email: <strong>mysite@.com</strong></i>'''
+   
+#     return HttpResponse(text)
+
+def about(request):
+    text = '''
+<i>Имя: <strong>Тимофей</strong></i><br>
+<i>Отчество: <strong>Сергеевич</strong></i><br>
+<i>Фамилия: <strong>Пармон</strong></i><br>
+<i>email: <strong>mysite@.com</strong></i>'''
+   
+    return HttpResponse(text)
+
+# def product(request):
+#     items = [
+#    {"id": 1, "name": "Кроссовки abibas"},
+#    {"id": 2, "name": "Куртка кожаная"},
+#    {"id": 3, "name": "Coca-cola 1 литр"},
+#    {"id": 4, "name": "Картофель фри"},
+#    {"id": 5, "name": "Кепка"},
+# ]
+
+#     return HttpResponse(items[1]['name'])
