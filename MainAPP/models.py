cea7aac3ead824c42f55d999607e4db80ef62eb0
Timofey <hunsolo94@gmail.com>
2024-12-13 16:30:18 +0300

    NEW

--- MainAPP/models.py
+++ MainAPP/models.py
@@ -1,7 +1,18 @@
 from django.db import models # type: ignore
 
+
+class Color(models.Model):
+   name = models.CharField(max_length=32)
+
+   def __repr__(self):
+      return f"Color: {self.name}"
+
 class Item(models.Model):
    name  = models.CharField(max_length=100)
    brand = models.CharField(max_length=100)
    count = models.PositiveIntegerField()
    properties = models.CharField(max_length=100, default=str)
+   colors = models.ManyToManyField(to=Color)
+
+   def __repr__(self):
+      return f"Item: {self.name}"
